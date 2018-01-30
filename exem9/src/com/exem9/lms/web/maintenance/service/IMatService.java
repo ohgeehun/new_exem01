37522b144415fba73ec76eac0f169bf3b0511fb1
ExemPark <ExemPark@ExemPark-PC>
2018-01-30 14:58:14 +0900

    1.최초 업로드

---
+++ exem9/src/com/exem9/lms/web/maintenance/service/IMatService.java
@@ -0,0 +1,30 @@
+package com.exem9.lms.web.maintenance.service;
+
+import java.util.List;
+
+import com.exem9.lms.web.common.bean.SupoBean;
+import com.exem9.lms.web.dbms.bean.DbmsBean;
+import com.exem9.lms.web.member.bean.MemberBean;
+import com.exem9.lms.web.team.bean.TeamBean;
+
+
+
+public interface IMatService {	
+	/*고객사 수정 이벤트 계약상태 리스트 데이터 가져오기 (SupoDao 연계) */
+	public List<SupoBean> getSupolevel() throws Throwable;
+
+	/*고객사 수정 이벤트 방문주기 리스트 데이터 가져오기 (SupoDao 연계) */
+	public List<SupoBean> getSupovisit(String supoId)throws Throwable;
+
+	/*  */
+	public List<MemberBean> getTeammember(String teamNm) throws Throwable ;
+	
+	/*고객사 수정 이벤트 담당부서(팀) 리스트 데이터 가져오기 (TeamDao 연계) */
+/*	public List<TeamBean> getuserteam(String userDept)throws Throwable;*/
+	
+	/*고객사 정보 등록시 담당팀 리스트 가져오기*/
+	public List<TeamBean> getdeptteam(String dbmsNm) throws Throwable;
+	
+	/*고객사 관리 검색 조건 중 업무 리스트 정보 가져오기 (DbmsDao 연계)*/
+	public List<DbmsBean> getdbms() throws Throwable;
+}
---
+++ exem9/src/com/exem9/lms/web/maintenance/service/MatService.java
@@ -0,0 +1,95 @@
+package com.exem9.lms.web.maintenance.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.directwebremoting.annotations.RemoteProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.exem9.lms.web.common.bean.SupoBean;
+import com.exem9.lms.web.common.dao.ICommonDao;
+import com.exem9.lms.web.dbms.bean.DbmsBean;
+import com.exem9.lms.web.dbms.dao.IDbmsDao;
+import com.exem9.lms.web.maintenance.dao.IMatDao;
+import com.exem9.lms.web.member.bean.MemberBean;
+import com.exem9.lms.web.member.dao.IMemberDao;
+import com.exem9.lms.web.team.bean.TeamBean;
+import com.exem9.lms.web.team.dao.ITeamDao;
+
+
+
+@RemoteProxy(name="IMatService")
+@Service(value="IMatService")
+public class MatService implements IMatService{
+	
+	@Autowired
+	public IMatDao iMatDao;
+	
+	@Autowired
+	public ICommonDao iCommonDao;
+	
+	@Autowired
+	public IMemberDao iMemberDao;
+	
+	@Autowired
+	public ITeamDao iTeamDao;
+	
+	@Autowired
+	public IDbmsDao iDbmsDao;
+	
+	public List<SupoBean> getSupolevel() throws Throwable {
+		// TODO Auto-generated method stub
+		return iCommonDao.getSupolevel();
+	}
+
+	public List<SupoBean> getSupovisit(String supoId) throws Throwable {
+		
+		HashMap params = new HashMap();
+		
+		if(supoId == ""){
+			params.put("supoId",supoId);
+		}else{
+			params.put("supoId",Integer.parseInt(supoId));
+		}
+		
+		return iCommonDao.getSupovisit(params);
+	}
+/*	public List getdbms() throws Throwable {
+		
+		// TODO Auto-generated method stub
+		return iDbmsDao.getdbms();
+	}*/
+
+	public List<MemberBean> getTeammember(String teamNm) throws Throwable {
+		
+		HashMap params = new HashMap();
+		params.put("teamNm",teamNm);
+		
+		return iMemberDao.getTeammember(params);
+	}
+
+/*	public List<TeamBean> getuserteam(String userDept) throws Throwable {
+		// TODO Auto-generated method stub
+		HashMap params = new HashMap();
+		params.put("userDept",userDept);
+		
+		
+		return iTeamDao.getuserteam(params);
+	}*/
+	
+	//고객사 정보 등록시 담당부서에 해당 하는 팀정보 가져오기
+	public List<TeamBean> getdeptteam(String dbmsNm) throws Throwable {
+		// TODO Auto-generated method stub
+		HashMap params = new HashMap();
+		params.put("dbmsNm",dbmsNm);
+		
+		
+		return iTeamDao.getdeptteam(params);
+	}
+	
+	public List<DbmsBean> getdbms() throws Throwable {
+		// TODO Auto-generated method stub
+		return iDbmsDao.getdbms();
+	}
+}
